0b87795c4a08ec5d220d96163f8617d3b28753b6
herwaldt <1995ekaj@gmail.com>
2020-01-04 14:46:02 -0500

    add mongoDB and mongoose

---
+++ models/Standard-Receipts.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const standardPOSchema = new Schema({
+  poNumber: String,
+  lineNumber: Number,
+  vendorName: String,
+  vendorSite: String,
+  itemNumber: String,
+  description: String,
+  poType: String,
+  unitPrice: Number,
+  buyer: String,
+  qtyTransacted: Number,
+  transactionDate: Date,
+  needByDate: Date,
+  promiseDate: Date
+});
+
+mongoose.model('standard-receipts', standardPOSchema);
