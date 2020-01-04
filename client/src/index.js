235cdefa18129b825c0ad9eaf39cb431202d1a0d
herwaldt <1995ekaj@gmail.com>
2020-01-04 13:01:43 -0500

    wire up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// arrow function is a dummy reducer until we have a real reducer to use
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>, 
+  document.querySelector('#root')
+);
