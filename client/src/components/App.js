a9cc9060f8d41d62220db208657af721401cb5f7
herwaldt <1995ekaj@gmail.com>
2020-01-05 09:08:20 -0500

    add route structure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Landing = () => <h2>Landing</h2>
+const SignUp = () => <h2>Sign Up Here</h2>
+const Scorecard = () => <h2>Scorecard</h2>
+const OTD = () => <h2>On Time Delivery</h2>
+const DPPM = () => <h2>Deffective Parts Per Million</h2>
+const Spend = () => <h2>Spend</h2>
 
 const App = () => {
   return (
-    <div>
-      Hi There!!
-    </div>
+    <>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/scorecard" component={Scorecard} />
+          <Route exact path="/scorecard/OTD" component={OTD} />
+          <Route exact path="/scorecard/DPPM" component={DPPM} />
+          <Route exact path="/scorecard/spend" component={Spend} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </BrowserRouter>
+    </>
   );
 };
 
