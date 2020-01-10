14f54c84c535b1d0abd90a37235bf44ad9d2ba3f
herwaldt <1995ekaj@gmail.com>
2020-01-09 21:04:00 -0500

    add update one filter

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const blanketReceipts = require('./models/Blanket-Receipts'); 
 const upload = multer({ dest: 'uploads/' });
 const fs = require('fs');
 const csvtojson = require('csvtojson');
+const blanketReceipts = require('./models/Blanket-Receipts');
+const receipts = require('./models/Receipts');
 const keys = require('./config/keys');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
 const app = express();
 
@@ -29,9 +30,11 @@ app.post('/api/upload', upload.single('statement'), (req, res) => {
           blanketReceipts.collection.insertMany(csvData, (error, result) => {
             if (error) {
               res.error('uhoh');
+              blanketReceipts.close();
             } else {
               console.log(result);
-              res.end(`inserted ${result.length}`);
+              res.end(`inserted ${result.insertedCount}`);
+              blanketReceipts.close();
             }
           });
         });
@@ -39,6 +42,64 @@ app.post('/api/upload', upload.single('statement'), (req, res) => {
       res.end('Well, there is no magic for those who don’t believe in it!');
     }
   });
+});
+
+
+app.post('/api/receipt', upload.single('statement'), (req, res) => {
+  console.log(`new upload = ${req.file.filename}\n`);
+  console.log(req.file);
+
+  fs.exists(req.file.path, (exists) => {
+    if (exists) {
+      csvtojson()
+        .fromFile(req.file.path)
+        .then((csvData) => {
+          console.log(csvData);
+          receipts.collection.insertMany(csvData, (error, result) => {
+            if (error) {
+              res.error('uhoh');
+              receipts.close();
+            } else {
+              console.log(result);
+              res.end(`inserted ${result.insertedCount}`);
+              receipts.close();
+            }
+          });
+        });
+    } else {
+      res.end('Well, there is no magic for those who don’t believe in it!');
+    }
+  });
+});
+
+app.post('/api/late', upload.single('statement'), (req, res) => {
+  console.log(`new upload = ${req.file.filename}\n`);
+  console.log(req.file);
+
+  fs.exists(req.file.path, (exists) => {
+    if (exists) {
+      csvtojson()
+        .fromFile(req.file.path)
+        .then((csvData) => {
+          console.log(csvData);
+
+          const filter = { "transactionId": "1000" };
+          const update = { $set: { "late": "Y" }};
+
+          receipts.collection.updateOne(filter, update);
+        });
+    } else {
+      res.end('Well, there is no magic for those who don’t believe in it!');
+    }
+  });
+});
+
+app.post('/api/later', (req, res) => {
+  const filter = { transactionId: '1001' };
+  const update = { $set: { late: 'Y', daysLate: 12 } };
+  console.log(req);
+  receipts.collection.updateOne(filter, update, { multi: true })
+    .then(res.end('inserted!'));
 });
 
 app.listen(5001);
