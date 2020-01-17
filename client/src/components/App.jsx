3082b6f812e445e221898dfb44f6e644b4be9c19
Jacob Herwaldt <jacobherwaldt@XRITE.COM>
2020-01-17 15:58:55 -0500

    add bar chart

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,8 +7,8 @@ import SignUp from './LoginComponents/SignUp';
 import Scorecard from './Scorecard';
 import ForgotPassword from './LoginComponents/ForgotPassword';
 import MonthSwitch from './ThemeComponents/MonthSwitch';
+import BarChart from './ThemeComponents/BarChart';
 
-const OTD = () => <h2>On Time Delivery</h2>;
 const DPPM = () => <h2>Deffective Parts Per Million</h2>;
 const Spend = () => <h2>Spend</h2>;
 
@@ -20,7 +20,7 @@ const App = () => (
         <Route exact path="/" component={Landing} />
         <Route path="/scorecard" component={MonthSwitch} />
         <Route exact path="/scorecard" component={Scorecard} />
-        <Route exact path="/scorecard/OTD" component={OTD} />
+        <Route exact path="/scorecard/OTD" component={BarChart} />
         <Route exact path="/scorecard/DPPM" component={DPPM} />
         <Route exact path="/scorecard/spend" component={Spend} />
         <Route exact path="/account/signup" component={SignUp} />
---
+++ client/src/components/ThemeComponents/BarChart.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card, Paper, Grid, Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(1),
+  },
+  paper: {
+    minWidth: '80vw',
+    margin: theme.spacing(1),
+  },
+  grid: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gridColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justify: 'center',
+    minWidth: '80vw',
+    margin: 'auto',
+  },
+  barchart: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+}));
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'OTD Percent',
+      backgroundColor: 'rgb(232,140,0)',
+      borderColor: 'rgb(232,140,0)',
+      borderWidth: 2,
+      hoverBackgroundColor: 'rgb(255,175,0)',
+      hoverBorderColor: 'rgb(245,245,245)',
+      data: [65, 59, 80, 81, 56, 55, 40],
+    }
+  ]
+};
+
+const options = {
+  maintainAspectRatio: false,
+  legend:{
+    display: false
+  },
+  responsive: true,
+  tooltips: {
+    mode: 'label'
+  },
+  elements: {
+    line: {
+      fill: false
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        gridLines: {
+          display: false
+        }
+      }
+    ],
+    yAxes: [
+      {
+        type: 'linear',
+        ticks: {
+          min: 0,
+          max: 100,
+          stepSize: 20,
+        },
+        display: true,
+        position: 'left',
+        labels: {
+          show: true
+        }
+      },
+    ]
+  }
+};
+
+export default function BarChart() {
+  const classes = useStyles();
+
+  return (
+    <Grid container direction="column" className={classes.grid}>
+        <Paper elevation={2} className={classes.paper}>
+            <Card className={classes.card}>
+              <Grid className={classes.gridColumn}>
+                <Typography variant="h4" component="h2">
+                  On-Time Delivery (OTD)
+                </Typography>
+                <Bar
+                data={data}
+                options={options}
+                height={300}
+                className={classes.barchart}
+                />
+              </Grid>
+            </Card>
+        </Paper>
+    </Grid>
+  );
+}
