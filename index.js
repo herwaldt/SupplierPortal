21f2136c60b9d2f18efa9966ad7d99c7afcd8a09
herwaldt <1995ekaj@gmail.com>
2020-03-04 18:01:10 -0500

    add heroku

--- index.js
+++ index.js
@@ -22,4 +22,5 @@ require('./routes/uploadData')(app);
 require('./routes/receiptData')(app);
 require('./routes/qualityData')(app);
 
-app.listen(5001);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT);
